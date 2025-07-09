530471ed5c9a858ec91a3bb357582adbe31a768f
JasonMKT <numberfive56789@gmail.com>
2025-07-09 07:21:51 +0700

    customize ui

--- src/modules/Elsa.Studio.Workflows.Core/UI/Providers/DefaultActivityDisplaySettingsProvider.cs
+++ src/modules/Elsa.Studio.Workflows.Core/UI/Providers/DefaultActivityDisplaySettingsProvider.cs
@@ -14,7 +14,7 @@ public class DefaultActivityDisplaySettingsProvider : IActivityDisplaySettingsPr
     {
         // Not Found Activity
         ["Elsa.NotFoundActivity"] = new(DefaultActivityColors.NotFound, ElsaStudioIcons.Heroicons.Exclamation),
-        
+
         // Branching
         ["Elsa.If"] = new(DefaultActivityColors.Branching, ElsaStudioIcons.Heroicons.Question),
         ["Elsa.FlowDecision"] = new(DefaultActivityColors.Branching, ElsaStudioIcons.Heroicons.Question),
@@ -22,27 +22,27 @@ public class DefaultActivityDisplaySettingsProvider : IActivityDisplaySettingsPr
         ["Elsa.FlowSwitch"] = new(DefaultActivityColors.Branching, ElsaStudioIcons.Tabler.SwitchDiagonal),
         ["Elsa.FlowJoin"] = new(DefaultActivityColors.Branching, ElsaStudioIcons.Tabler.GitMerge),
         ["Elsa.FlowFork"] = new(DefaultActivityColors.Branching, ElsaStudioIcons.Tabler.GitFork),
-        
+
         // Composition
         ["Elsa.Complete"] = new(DefaultActivityColors.Composition, ElsaStudioIcons.Tabler.CheckCircle),
-        ["Elsa.SetOutput"] = new (DefaultActivityColors.Composition, Icons.Material.Outlined.Output),
-        ["Elsa.DispatchWorkflow"] = new (DefaultActivityColors.Composition, Icons.Material.Outlined.Commit),
-        ["Elsa.BulkDispatchWorkflows"] = new (DefaultActivityColors.Composition, Icons.Material.Outlined.Share),
-        ["Elsa.ExecuteWorkflow"] = new (DefaultActivityColors.Composition, Icons.Material.Outlined.Terminal),
-        
+        ["Elsa.SetOutput"] = new(DefaultActivityColors.Composition, Icons.Material.Outlined.Output),
+        ["Elsa.DispatchWorkflow"] = new(DefaultActivityColors.Composition, Icons.Material.Outlined.Commit),
+        ["Elsa.BulkDispatchWorkflows"] = new(DefaultActivityColors.Composition, Icons.Material.Outlined.Share),
+        ["Elsa.ExecuteWorkflow"] = new(DefaultActivityColors.Composition, Icons.Material.Outlined.Terminal),
+
         // Console
         ["Elsa.WriteLine"] = new(DefaultActivityColors.Console, ElsaStudioIcons.Tabler.Pencil),
         ["Elsa.ReadLine"] = new(DefaultActivityColors.Console, ElsaStudioIcons.Tabler.Text),
-        
+
         // Email
         ["Elsa.SendEmail"] = new(DefaultActivityColors.Email, Icons.Material.Outlined.Email),
-        
+
         // Flowchart
         ["Elsa.Flowchart"] = new(DefaultActivityColors.Flowchart, ElsaStudioIcons.Tabler.GitFork),
         ["Elsa.FlowNode"] = new(DefaultActivityColors.Flowchart, ElsaStudioIcons.Tabler.Hexagon),
         ["Elsa.Start"] = new(DefaultActivityColors.Flowchart, Icons.Material.Outlined.Start),
         ["Elsa.End"] = new(DefaultActivityColors.Flowchart, Icons.Material.Outlined.OutlinedFlag),
-        
+
         // HTTP
         ["Elsa.HttpEndpoint"] = new(DefaultActivityColors.Http, ElsaStudioIcons.Tabler.Cloud),
         ["Elsa.WriteHttpResponse"] = new(DefaultActivityColors.Http, ElsaStudioIcons.Heroicons.PencilPaper),
@@ -50,14 +50,14 @@ public class DefaultActivityDisplaySettingsProvider : IActivityDisplaySettingsPr
         ["Elsa.SendHttpRequest"] = new(DefaultActivityColors.Http, ElsaStudioIcons.Tabler.World),
         ["Elsa.FlowSendHttpRequest"] = new(DefaultActivityColors.Http, ElsaStudioIcons.Tabler.World),
         ["Elsa.DownloadHttpFile"] = new(DefaultActivityColors.Http, @Icons.Material.Outlined.CloudDownload),
-        
+
         // Looping
         ["Elsa.While"] = new(DefaultActivityColors.Looping, ElsaStudioIcons.Tabler.RepeatOne),
         ["Elsa.ForEach"] = new(DefaultActivityColors.Looping, ElsaStudioIcons.Tabler.RepeatOne),
         ["Elsa.For"] = new(DefaultActivityColors.Looping, ElsaStudioIcons.Tabler.RepeatOne),
         ["Elsa.ParallelForEach"] = new(DefaultActivityColors.Looping, ElsaStudioIcons.Tabler.RepeatOne),
         ["Elsa.Break"] = new(DefaultActivityColors.Looping, ElsaStudioIcons.Tabler.Back1),
-        
+
         // Primitives
         ["Elsa.SetVariable"] = new(DefaultActivityColors.Primitives, ElsaStudioIcons.Tabler.Pencil),
         ["Elsa.SetName"] = new(DefaultActivityColors.Primitives, ElsaStudioIcons.Tabler.Italic),
@@ -67,18 +67,26 @@ public class DefaultActivityDisplaySettingsProvider : IActivityDisplaySettingsPr
         ["Elsa.RunTask"] = new(DefaultActivityColors.Primitives, Icons.Material.Outlined.Settings),
         ["Elsa.PublishEvent"] = new(DefaultActivityColors.Primitives, Icons.Material.Outlined.FlashOn),
         ["Elsa.Event"] = new(DefaultActivityColors.Primitives, Icons.Material.Outlined.FlashOn),
-        
+
         // Timers
         ["Elsa.Timer"] = new(DefaultActivityColors.Timer, Icons.Material.Outlined.Timer),
         ["Elsa.Cron"] = new(DefaultActivityColors.Timer, Icons.Material.Outlined.Timer),
         ["Elsa.Delay"] = new(DefaultActivityColors.Timer, Icons.Material.Outlined.RotateLeft),
         ["Elsa.StartAt"] = new(DefaultActivityColors.Timer, Icons.Material.Outlined.CalendarMonth),
-        
+
         // Scripting
         ["Elsa.RunJavaScript"] = new(DefaultActivityColors.Scripting, Icons.Material.Outlined.Javascript),
 
         // Azure Service Bus
         ["Elsa.AzureServiceBus.MessageReceived"] = new("#a21caf", ElsaStudioIcons.Heroicons.Incoming),
         ["Elsa.AzureServiceBus.SendMessage"] = new("#a21caf", ElsaStudioIcons.Heroicons.Outgoing),
+
+        //Custom Activities
+        ["Elsa.CustomActivities.Activities.PrintMessage"] = new(DefaultActivityColors.CustomActivities, ElsaStudioIcons.Heroicons.ArrowDownLeft),
+        ["Elsa.CustomActivities.Activities.SimpleEvent"] = new(DefaultActivityColors.Primitives, ElsaStudioIcons.Heroicons.Link),
+        ["Elsa.CustomActivities.Activities.SelectItemListActivity"] = new(DefaultActivityColors.Timer, ElsaStudioIcons.Heroicons.Send),
+        ["Elsa.CustomActivities.Activities.RandomNumberActivity"] = new(DefaultActivityColors.CustomActivities, ElsaStudioIcons.Heroicons.Launch),
+
+        
     };
 }
